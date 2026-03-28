c6b8a2269712dd6a04f483793a57a928285c22bd
Johnnyevans32 <evanseburu32@gmail.com>
2026-03-28 10:57:10 +0100

    fix

---
+++ blog/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Post, Comment
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+
+admin.site.register(Post, PostAdmin)
